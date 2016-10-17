e40716a9b31d264261e8f2d8f7614a5340b6385f
Yuuta Togashi <romasss96@gmail.com>
2016-10-18 01:51:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,344 @@
+/**
+ * Created by kamina on 07.10.16.
+ */
+
+var data = {'cheap': [{
+    'title': 'Ноутбук Lenovo IdeaPad 110-15ACL (80TJ004ARK)',
+    'image': './images/cheap/1.jpg',
+    'price': 22000,
+    'description': {
+        'Операционная система': 'Windows 10',
+        'Процессор': 'AMD A4, 2 ядра',
+        'Видеокарта': 'AMD Radeon R3',
+        'Оперативная память (RAM)': '4 ГБ',
+        'Диагональ экрана': '15.6"(39.6 см)',
+        'Жесткий диск (HDD)': '500 ГБ',
+        'Работа от аккумулятора': 'до 4 часов',
+        'Емкость аккумулятора': '2200 мАч',
+        'Цвет': 'черный',
+        'Габаритные размеры (В*Ш*Г)': '23*378*265 мм',
+        'Вес': '2.31 кг'
+
+    }
+},
+    {
+        'title': 'Ноутбук ASUS X552WA-BING-SX137B',
+        'image': './images/cheap/2.jpg',
+        'price': 17000,
+        'description': {
+            'Операционная система': 'Windows 8.1',
+            'Процессор': 'AMD	E1-2100 1ГГц, 2 ядра',
+            'Видеокарта': 'AMD Radeon HD 8210G',
+            'Оперативная память (RAM)': '2 ГБ',
+            'Диагональ экрана': '15.6"(39.6 см)',
+            'Жесткий диск (HDD)': '500 ГБ',
+            'Работа от аккумулятора': 'до 3 часов',
+            'Емкость аккумулятора': '2500 мАч',
+            'Цвет': 'черный',
+            'Габаритные размеры (В*Ш*Г)': '34*380*251 мм',
+            'Вес': '2.45 кг'
+
+        }
+    },
+    {
+        'title': 'Acer Aspire ES1-520-38XM NX.G2JER.015',
+        'image': './images/cheap/3.jpg',
+        'price': 17000,
+        'description': {
+            'Операционная система': 'Windows 10',
+            'Процессор': 'AMD E1-2500 1.4ГГц, 2 ядра',
+            'Видеокарта': 'AMD Radeon HD 8210G',
+            'Оперативная память (RAM)': '2 ГБ',
+            'Диагональ экрана': '15.6"(39.6 см)',
+            'Жесткий диск (HDD)': '500 ГБ',
+            'Работа от аккумулятора': 'до 4 ч 30 мин',
+            'Емкость аккумулятора': '3220 мАч',
+            'Цвет': 'черный',
+            'Габаритные размеры (В*Ш*Г)': '26*382*258 мм',
+            'Вес': '2.4 кг'
+
+        }
+    },
+    {
+        'title': 'Ноутбук-трансформер ASUS TP201SA-FV0009T',
+        'image': './images/cheap/4.jpg',
+        'price': 29000,
+        'description': {
+            'Операционная система': 'Windows 10',
+            'Процессор': 'Intel Celeron N3060 1.6ГГц, 2 ядра',
+            'Видеокарта': 'Intel HD Graphics',
+            'Оперативная память (RAM)': '2 ГБ',
+            'Диагональ экрана': '11.6"(29.4 см)',
+            'Жесткий диск (HDD)': '500 ГБ',
+            'Работа от аккумулятора': 'до 11 часов',
+            'Цвет': 'темно-серый',
+            'Габаритные размеры (В*Ш*Г)': '22*297*201 мм',
+            'Вес': '2.4 кг',
+
+        }
+    },
+    {
+        'title': 'Ноутбук-трансформер Lenovo YOGA 300-11IBR (80M100K8RK)',
+        'image': './images/cheap/5.jpg',
+        'price': 27000,
+        'description': {
+            'Операционная система': 'Windows 10',
+            'Процессор': 'Intel Celeron N3060 1.6ГГц, 2 ядра',
+            'Видеокарта': 'Intel HD Graphics',
+            'Оперативная память (RAM)': '2 ГБ',
+            'Диагональ экрана': '11.6"(29.4 см)',
+            'Жесткий диск (HDD)': '500 ГБ',
+            'Работа от аккумулятора': 'до 2 часов',
+            'Емкость аккумулятора': '1500 мАч',
+            'Цвет': 'белый/черный',
+            'Габаритные размеры (В*Ш*Г)': '22*299*209 мм',
+            'Вес': '1.4 кг'
+
+        }
+    }],
+    'uni': [{
+        'title': 'ASUS N551JB-XO044T',
+        'image': './images/universal/1.jpg',
+        'price': 56000,
+        'description': {
+            'Операционная система': 'Windows 10',
+            'Процессор': 'Intel Core i5-4200H 2.8ГГц, 2 ядра',
+            'Видеокарта': 'NVIDIA GeForce 940M 2ГБ',
+            'Оперативная память (RAM)': '8 ГБ',
+            'Диагональ экрана': '15.6"(39.6 см)',
+            'Жесткий диск (HDD)': '1 ТБ',
+            'Работа от аккумулятора': 'до 5 часов',
+            'Емкость аккумулятора': '5200 мАч',
+            'Цвет': 'стальной',
+            'Габаритные размеры (В*Ш*Г)': '32*383*255 мм',
+            'Вес': '2.7 кг'
+        }
+    },
+        {
+            'title': 'Acer Aspire E5-573-57Y6 NX.MVHER.039',
+            'image': './images/universal/2.jpg',
+            'price': 37000,
+            'description': {
+                'Операционная система': 'Windows 10',
+                'Процессор': 'Intel Core i5-5200U 2.2ГГц, 2 ядра',
+                'Видеокарта': 'Intel HD Graphics 5500',
+                'Оперативная память (RAM)': '4 ГБ',
+                'Диагональ экрана': '15.6"(39.6 см)',
+                'Жесткий диск (HDD)': '500 ГБ',
+                'Работа от аккумулятора': 'до 5 часов',
+                'Емкость аккумулятора': '2520 мАч',
+                'Цвет': 'темно-серый',
+                'Габаритные размеры (В*Ш*Г)': '29*381*256 мм',
+                'Вес': '2.4 кг'
+            }
+        },
+        {
+            'title': 'Ноутбук-трансформер Lenovo Yoga 500 (80R500BTRK)',
+            'image': './images/universal/3.jpg',
+            'price': 61000,
+            'description': {
+                'Операционная система': 'Windows 10',
+                'Процессор': 'Intel Core i5-6200U 2.3ГГц, 2 ядра',
+                'Видеокарта': 'NVIDIA GeForce 940M 2ГБ',
+                'Оперативная память (RAM)': '4 ГБ',
+                'Диагональ экрана': '15.6"(39.6 см)',
+                'Жесткий диск (HDD)': '1 ТБ',
+                'Работа от аккумулятора': 'до 8 часов',
+                'Емкость аккумулятора': '4100 мАч',
+                'Цвет': 'белый/черный',
+                'Габаритные размеры (В*Ш*Г)': '22*340*235 мм',
+                'Вес': '1.8 кг'
+            }
+        },
+        {
+            'title': 'Ноутбук-трансформер Lenovo Yoga 700 14 (80QD006SRK)',
+            'image': './images/universal/4.jpg',
+            'price': 79000,
+            'description': {
+                'Операционная система': 'Windows 10',
+                'Процессор': 'Intel Core i5-6200U 2.3ГГц, 2 ядра',
+                'Видеокарта': 'Intel HD Graphics 520',
+                'Оперативная память (RAM)': '8 ГБ',
+                'Диагональ экрана': '14"(35.5 см)',
+                'Жесткий диск (HDD)': '500 ГБ',
+                'Работа от аккумулятора': 'до 9 часов',
+                'Емкость аккумулятора': '6000 мАч',
+                'Цвет': 'белый/черный',
+                'Габаритные размеры (В*Ш*Г)': '18*335*229 мм',
+                'Вес': '1.7 кг'
+            }
+        }],
+    'game':[
+        {
+            'title': 'Ноутбук-трансформер Lenovo Yoga 700 14 (80QD006SRK)',
+            'image': './images/game/1.jpg',
+            'price': 78000,
+            'description': {
+                'Операционная система': 'Windows 10',
+                'Процессор': 'Intel Core i7-6700HQ 2.6ГГц, 4 ядра',
+                'Видеокарта': 'NVIDIA GeForce GTX960M 2GB',
+                'Оперативная память (RAM)': '8 ГБ',
+                'Диагональ экрана': '17.3"(43.9 см)',
+                'Жесткий диск (HDD)': '1 ТБ',
+                'Работа от аккумулятора': 'до 3 часов',
+                'Емкость аккумулятора': '3200 мАч',
+                'Цвет': 'графит./черный',
+                'Габаритные размеры (В*Ш*Г)': '32*415*274 мм',
+                'Вес': '3 кг'
+            }
+        },
+        {
+            'title': 'Lenovo IdeaPad Y700 17 (80Q00018RK)',
+            'image': './images/game/2.jpg',
+            'price': 104000,
+            'description': {
+                'Операционная система': 'Windows 10',
+                'Процессор': 'Intel Core i7-6700HQ 2.6ГГц, 4 ядра',
+                'Видеокарта': 'NVIDIA GeForce GTX960M 4GB',
+                'Оперативная память (RAM)': '12 ГБ',
+                'Диагональ экрана': '17.3"(43.9 см)',
+                'Жесткий диск (HDD)': '256ГБ SSD + 1ТБ',
+                'Работа от аккумулятора': 'до 4 часов',
+                'Емкость аккумулятора': '4100 мАч',
+                'Цвет': 'черный',
+                'Габаритные размеры (В*Ш*Г)': '28*423*305 мм',
+                'Вес': '3.5 кг'
+            }
+        },
+        {
+            'title': 'Acer Aspire V15 Nitro VN7-592G-77A6 NH.G6JER.002',
+            'image': './images/game/3.jpg',
+            'price': 104000,
+            'description': {
+                'Операционная система': 'Windows 10',
+                'Процессор': 'Intel Core i7-6700HQ 2.6ГГц, 4 ядра',
+                'Видеокарта': 'NVIDIA GeForce GTX960M 4GB',
+                'Оперативная память (RAM)': '16 ГБ',
+                'Диагональ экрана': '15.6"(39.6 см)',
+                'Жесткий диск (HDD)': '256ГБ SSD + 1ТБ',
+                'Работа от аккумулятора': 'до 5 часов',
+                'Емкость аккумулятора': '4450 мАч',
+                'Цвет': 'черный',
+                'Габаритные размеры (В*Ш*Г)': '24*390*258 мм',
+                'Вес': '2.4 кг'
+            }
+        }
+    ]
+};
+
+var categories = {'cheap': 'Чтобы вы стали обладателем одним из лучших ноутбуков для учебы, не обязательно переплачивать. Купить недорогой ноутбук для школьника или студента можно за весьма умеренную цену. Встроенных возможностей будет достаточно для неискушенного пользователя. Так же мы представляем модель ноутбук-трансформер. Несмотря на немного более высокую цену, пользоваться таким ноутбуком будет значительно удобнее.',
+                  'uni': 'Наши ноутбуки могут больше, чем быть просто компьютером. Это то,что нужно для работы,игр и развлечений,то что нужно именно тебе для яркой и насыщенной жизни. А трансформеры становятся всё мощнее, производительнее и удобнее, поражая своей пластичностью, подобно человеческому телу.( картинка сравнение компа и человека- йога )',
+                  'game': 'Создавая игровые компьютер, мы поставили перед собой цель, которая казалась невозможной. Игровой процессор нового уровня, мощная видеокарта, удобство и разнообразите функционала. Это ноутбук будущего. На каждом игровом компьютере установлены приложения для работы и творчества. Ваш игровой компьютер не просто наполнен функциями — он готов ко всему.'};
+function getDescription(descDict){
+    var res = [];
+    for(var key in descDict){
+        res.push(<p><span className="description_title">{key}</span>:
+            <span className="description_title">{descDict[key]}</span></p>)
+    }
+    console.log(res);
+    return res;
+}
+
+var chosen_category = 'chea';
+
+
+
+var PicModal = React.createClass({
+    render: function() {
+        return (
+            <div className="modal fade" id={'i' + this.props.image_src[this.props.image_src.length - 5]} tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        </div>
+                        <div className="modal-body">
+                            <img src={this.props.image_src}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var PCDescription = React.createClass({
+    render: function() {
+        return (
+                <div className="row pcbox">
+                    <div className="col-lg-3 col-md-3 col-sm-4">
+                        <img className="img-circle image-pc" src={this.props.image_src} data-toggle="modal"
+                             data-target={'#i' + this.props.image_src[this.props.image_src.length - 5]}/>
+                        <PicModal image_src={this.props.image_src}/>
+                    </div>
+                    <div className="col-lg-9 col-md-9 col-sm-8">
+                        <h3>{this.props.pc_title}</h3>
+                        <h4 style={{color: 'blue'}}>Цена: {this.props.price_pc}</h4>
+                        <ul>
+                            {
+                                getDescription(this.props.pc_description)
+                            }
+                        </ul>
+                    </div>
+                </div>
+        );
+    }
+});
+
+var PCDescriptionBox = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {
+                    this.props.model.map(function(el){
+                        return <PCDescription image_src={el.image} pc_title={el.title} pc_description={el.description}
+                                                price_pc={el.price}>
+                        </PCDescription>
+                    })
+                }
+            </div>
+        );
+    }
+});
+
+var CategoryPic = React.createClass({
+    onClick: function(event){
+        change_category(this.props.category);
+    },
+    render: function() {
+        return (
+            <div className="col-md-4 col-lg-4 category-box">
+                <a data-toggle="collapse" data-target="#collapsePlace" aria-expanded="false" aria-controls="collapseExample"
+                   onClick={this.onClick}>
+                    <img src={this.props.image} className="image-category"/>
+                </a>
+                <h3>{this.props.name}</h3>
+                <div className="category-description">
+                    <p>{categories[this.props.category]}</p>
+                </div>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<div className="col-centered">
+    <CategoryPic image="images/hand-money.png" category="cheap" name="Бюджетные"></CategoryPic>
+    <CategoryPic image="images/mecha.png" category="uni" name="Универсальные"></CategoryPic>
+    <CategoryPic image="images/gamepad-icon.png" category="game" name="Игровые"></CategoryPic>
+    </div>,
+    document.getElementById('bla'));
+
+
+function change_category(new_cat){
+    console.log('clicked');
+    if (chosen_category != new_cat){
+        chosen_category = new_cat;
+        console.log(chosen_category);
+        ReactDOM.render(<PCDescriptionBox model={data[chosen_category]}></PCDescriptionBox>,
+            document.getElementById('category-place'));
+    }
+};
+
+
+
+
